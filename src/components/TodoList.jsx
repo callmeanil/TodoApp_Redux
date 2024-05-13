1d7419eb0da9ec1e9d36822f5ea68748ca27cc9f
Anil Kumar <82893113+callmeanil@users.noreply.github.com>
2024-05-14 03:11:11 +0530

    Add files via upload

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,31 @@
+import { useSelector } from "react-redux";
+import TodoItem from "./TodoItem";
+
+const TodoList = () => {
+  const filteredTodos = useSelector((state) => {
+    const todos = state.todos;
+    const filter = state.filter;
+   
+    return todos.filter((todo) => {
+      const matchesFilter = (filter === 'COMPLETED' && todo.completed) ||
+        (filter === 'INCOMPLETE' && !todo.completed) ||
+        filter === 'ALL';
+
+     
+      return matchesFilter;
+    });
+  });
+
+  console.log('Filtered Todos:', filteredTodos);
+
+  return (
+    <ul>
+      <li className="my-2 text-sm italic">All Your Todo Here...  ( Created By Anil Kumar)</li>
+      {filteredTodos.map((todo, index) => (
+        <TodoItem key={index} todo={todo} index={index} />
+      ))}
+    </ul>
+  );
+};
+
+export default TodoList;
